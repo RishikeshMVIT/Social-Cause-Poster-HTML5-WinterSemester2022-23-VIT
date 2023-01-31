c41a0c73f542b1b4aca838510ac0e12d48a458a9
Rishikesh M <rishikesh.m2021@vitbhopal.ac.in>
2023-01-31 17:13:30 +0530

    Update

--- script.js
+++ script.js
@@ -1,50 +1,57 @@
+//#region Spinning lines
+let canvas;
+let ctx;
+let w, h;
 
-
-//#region ThemeSwitching
-
-const darkButton = document.getElementById('dark');
-const lightButton = document.getElementById('light');
-const solarButton = document.getElementById('solar');
-const body = document.body;
-
-
-// Apply the cached theme on reload
-
-const theme = localStorage.getItem('theme');
-const isSolar = localStorage.getItem('isSolar');
-
-if (theme) {
-  body.classList.add(theme);
-  isSolar && body.classList.add('solar');
+function setup() {
+  canvas = document.querySelector("#spinninglines");
+  ctx = canvas.getContext("2d");
+  resize();
+  window.addEventListener("resize", resize);
 }
 
-// Button Event Handlers
+function resize() {
+  w = canvas.width = window.innerWidth;
+  h = canvas.height = window.innerHeight;
+}
 
-darkButton.onclick = function(){
-	body.classList.replace('light', 'dark');
-	localStorage.setItem('theme', 'dark');
-};
+function draw(now) {
+  requestAnimationFrame(draw);
+  ctx.fillRect(0, 0, w, h);
+  
+  ctx.strokeStyle = "white";
+  const r = Math.min(w, h) * 0.35;
+  const angleOffset = now * 0.0018;  
+  circle(r, angleOffset);
+  circle(r*0.8, angleOffset);
+  circle(r*0.6, angleOffset);
+  circle(r*0.4, angleOffset);
+  circle(r*0.2, angleOffset);
+}
 
-lightButton.onclick = function(){
-	body.classList.replace('dark', 'light');
-	localStorage.setItem('theme', 'light');
-};
-
-solarButton.onclick = function(){
-
-  if (body.classList.contains('solar')) {
-    
-    body.classList.remove('solar');
-	solarButton.textContent = "solarize";
-    localStorage.removeItem('isSolar');
-
-  } else {
-
-    body.classList.add('solar');
-	solarButton.textContent = "normalize";
-    localStorage.setItem('isSolar', true);
-
+function circle(r, angleOffset) {
+  const nrOfLines = 64;
+  const l = 2 * r * Math.PI / nrOfLines;
+  for(let i = 0; i < nrOfLines; i++) {
+    const angle1 = Math.PI * 2 * i / nrOfLines;
+    const x1 = Math.cos(angle1) * r + w / 2;
+    const y1 = Math.sin(angle1) * r + h / 2;
+    line(x1, y1, l, angle1 * 8 + angleOffset);
   }
-};
+}
 
+function line(x, y, l, angle) {
+  ctx.save();
+  ctx.translate(x, y);
+  ctx.rotate(angle);
+  ctx.beginPath();
+  ctx.moveTo(-l/2, 0);
+  ctx.lineTo(l/2, 0);
+  ctx.stroke();
+  ctx.restore();
+}
+
+setup();
+draw(performance.now());
 //#endregion
+
